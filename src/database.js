d0f2a2c7e54eb1e0c3fb451f0ca3336b34701ee0
rjpaternina <rjpaternina@gmail.com>
2021-12-04 00:21:59 -0500

    Uri Atlas  conection

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost:27017/Market';
+//const uri = 'mongodb://localhost:27017/Market';
+//const uri = 'mongodb+srv://juan:123@marketplace.tklcz.mongodb.net/marketplace?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://farmerplace458:wi5oleaaicBEkbRX@cluster0.rmg17.mongodb.net/cluster0?retryWrites=true&w=majority';
 
 const options = { useNewUrlParser:true, useUnifiedTopology:true}
 
